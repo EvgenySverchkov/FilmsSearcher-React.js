250c69144abb1c14f609011428e88c46ff55071c
EvgenySverchkov <47426436+EvgenySverchkov@users.noreply.github.com>
2019-10-26 17:09:06 +0300

    add new component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import SearchField from './components/SearchField.js';
+import request from './services/getMovies.js';
+import MoviePoster from './components/MoviePoster.js';
 import './App.css';
 
 function App() {
-  return (<div><SearchField/></div>);
+
+  return (<>
+    <SearchField/>
+    </>);
 }
 
 export default App;
