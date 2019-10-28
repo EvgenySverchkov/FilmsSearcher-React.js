b6aee2110d84c27f4ff50012b25664fa1eb06d49
EvgenySverchkov <47426436+EvgenySverchkov@users.noreply.github.com>
2019-10-28 21:50:24 +0200

    added search field

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-export default function SearchField(){
-  return(<input placeholder="Enter movie name"/>);
+export default function SearchField({handlerClick}){
+  return(
+    <form id="search_form" onSubmit={(e)=>{e.preventDefault(); handlerClick(e.target.elements.movieName.value)}}>
+      <input id="search_field" type="text" name="movieName" placeholder="Enter movie title"/>
+      <input id="search_button" type="submit" value="Find" />
+    </form>
+  );
 }
