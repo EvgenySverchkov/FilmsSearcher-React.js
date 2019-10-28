4baac54ff24bc6d1ac9247ae238ff24ba03d3750
EvgenySverchkov <47426436+EvgenySverchkov@users.noreply.github.com>
2019-10-28 21:52:00 +0200

    added component MoviePoster. It contains data of movie(poster, title)

--- src/components/MoviePoster.js
+++ src/components/MoviePoster.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
-export default function({movieObj}){
-  return (<div style={{width: 50, height: 50, background: 'black'}}></div>);
+export default function({movieObj, imgUrl, title}){
+  function select_img(img){
+    if(img!=="N/A"){
+      return (<img id="poster_img" src={img} alt={title}/>)
+    }else{
+      return (<img id="none_poster_img" src={require('../loader.png')} alt={title}/>)
+    }
+  }
+
+  return (
+    <div id="poster_wrap">
+      <p>{title}</p>
+      {select_img(imgUrl)}
+    </div>
+  );
 }
