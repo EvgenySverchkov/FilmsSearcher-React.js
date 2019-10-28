539839fb109b73e57f5a5cd1836c14c37093f82f
EvgenySverchkov <47426436+EvgenySverchkov@users.noreply.github.com>
2019-10-28 21:48:34 +0200

    added request for data (data is taken from 'omdbapi' by token)

--- src/services/getMovies.js
+++ src/services/getMovies.js
@@ -1,7 +1,8 @@
+/*let delay = ()=>new Promise(res=>setTimeout(res, 15000));*/
 export default function(movieName){
   let result = fetch(`http://omdbapi.com/?apikey=4a3b711b&s=${movieName}`)
     .then((data)=>data.json())
-    .then(data=>data.Search);
-
+    .then(data=>!data.Search ? undefined : data.Search);
+/*let delayFetch = delay().then(()=>result);*/
     return result;
 }
